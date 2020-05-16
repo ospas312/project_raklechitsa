dec15186d4aa5838c7a26f2122f10d744b408d72
IxxyDev <denisenkoforwork@gmail.com>
2020-05-16 23:15:30 +0300

    fix: lots of fixes after review

--- components/Instagram.vue
+++ components/Instagram.vue
@@ -1,30 +1,51 @@
 <template>
   <section class="instagram">
-    <div class="instagram__text">
-      <h2 class="instagram__title">Инстаграм</h2>
-      <p class="instagram__description">
-        Два раза в неделю мы просматриваем все посты по хештегу #этонелечится.
-        Все истории, где нет нецензурных выражений и запрещенного контента
-        попадают сюда. Следите за правильным написанием хештега, чтобы мы не
-        пропустили вашу историю.
-      </p>
-    </div>
-    <div class="instagram__stories">
-      <instagram-story
-        v-for="instagramStory in instagramStories.slice(0, 8)"
-        :key="instagramStory.id"
-        :picUrl="instagramStory.instagramPicUrl"
-        :instagramPicAlt="instagramStory.instagramStoryImageAltData"
-      />
-    </div>
+    <container class="instagram__container">
+      <slogan block>
+        <template #slogan-text
+          >РАССКАЗЫВАЙТЕ ВАШИ ИСТОРИИ В ИНСТАГРАМ&nbsp;</template
+        >
+        <template #slogan-hashtag>#ЭТОНЕЛЕЧИТСЯ</template>
+      </slogan>
+      <div class="instagram__content">
+        <div class="instagram__text">
+          <h2 class="instagram__title">
+            <a
+              class="instagram__link"
+              href="https://www.instagram.com/raklechitsa/"
+              target="_blank"
+              >Инстаграм</a
+            >
+          </h2>
+          <p class="instagram__description">
+            Два раза в неделю мы просматриваем все посты по хештегу
+            #этонелечится. Все истории, где нет нецензурных выражений и
+            запрещенного контента попадают сюда. Следите за правильным
+            написанием хештега, чтобы мы не пропустили вашу историю.
+          </p>
+        </div>
+        <div class="instagram__stories">
+          <instagram-story
+            v-for="instagramStory in instagramStories.slice(0, 8)"
+            :key="instagramStory.id"
+            :picUrl="instagramStory.instagramPicUrl"
+            :instagramPicAlt="instagramStory.instagramStoryImageAltData"
+          />
+        </div>
+      </div>
+    </container>
   </section>
 </template>
 
 <script>
 import InstagramStory from '@/components/ui/InstagramStory.vue';
+import Container from '@/components/ui/Container.vue';
+import Slogan from '@/components/ui/Slogan.vue';
 export default {
   components: {
+    container: Container,
     'instagram-story': InstagramStory,
+    slogan: Slogan,
   },
   data() {
     return {
@@ -76,22 +97,39 @@ export default {
 </script>
 
 <style scoped>
-.instagram {
-  max-width: 1440px;
-  width: 100%;
-  padding: 100px 60px;
+.instagram__container {
+  padding-bottom: 100px;
+}
+
+.instagram__content {
+  margin-top: 100px;
   display: flex;
-  justify-content: space-between;
-  box-sizing: border-box;
+}
+
+.instagram__text {
+  margin-right: 24px;
+  min-width: 413px;
 }
 
 .instagram__title {
+  margin: 0;
   font-weight: 600;
-  border-bottom: 2px solid black;
-  max-width: 130px;
+  font-size: 32px;
+  line-height: 36px;
+}
+
+.instagram__link {
+  color: black;
+  text-decoration: none;
+  border-bottom: 3px solid black;
+}
+
+.instagram__link:hover {
+  opacity: 0.6;
 }
 
 .instagram__description {
+  margin: 32px 0 0;
   max-width: 340px;
   font-weight: normal;
   font-size: 18px;
@@ -100,66 +138,91 @@ export default {
 }
 
 .instagram__stories {
+  width: 100%;
   display: grid;
   grid-template-columns: repeat(4, 1fr);
-  grid-template-rows: repeat(2, 1fr);
   grid-gap: 30px;
 }
 
 @media screen and (max-width: 1280px) {
-  .instagram {
-    padding: 80px 50px;
+  .instagram__stories {
+    grid-gap: 27px;
+  }
+
+  .instagram__title {
+    max-width: 367px;
+    font-size: 28px;
+    line-height: 32px;
   }
 
   .instagram__description {
+    margin-top: 30px;
+    max-width: 305px;
     font-size: 16px;
     line-height: 20px;
   }
 }
 
 @media screen and (max-width: 1024px) {
-  .instagram {
-    padding: 80px 50px;
-  }
-  .instagram__description {
-    font-size: 13px;
-    line-height: 16px;
+  .instagram__stories {
+    grid-gap: 20px;
   }
 
-  .instagram__text {
-    margin-right: 60px;
+  .instagram__title {
+    max-width: 288px;
+    font-size: 24px;
+    line-height: 28px;
+  }
+
+  .instagram__description {
+    margin-top: 20px;
+    max-width: 260px;
+    font-size: 13px;
+    line-height: 16px;
   }
 }
 
 @media screen and (max-width: 768px) {
-  .instagram {
+  .instagram__content {
     flex-direction: column;
   }
+
+  .instagram__stories {
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 20px;
+  }
+
+  .instagram__title {
+    max-width: 380px;
+    margin: auto;
+    text-align: center;
+  }
+
+  .instagram__description {
+    margin: 26px auto 0;
+    max-width: 380px;
+  }
+
   .instagram__text {
-    margin: 0 auto 60px;
+    margin-bottom: 60px;
   }
 }
 
-@media screen and (max-width: 480px) {
-  .instagram {
-    padding: 60px 30px;
-  }
-
+@media screen and (max-width: 425px) {
   .instagram__stories {
     grid-template-columns: repeat(2, 1fr);
-    grid-template-rows: repeat(4, 1fr);
+    grid-gap: 10px;
   }
-}
 
-@media screen and (max-width: 320px) {
-  .instagram {
-    padding: 50px 15px;
+  .instagram__title {
+    margin: 0;
+    max-width: 367px;
+    font-size: 18px;
+    line-height: 21px;
   }
-}
 
-@media screen and (min-width: 320px) and (max-width: 1440px) {
-  .instagram__stories {
-    grid-gap: calc(10px + (30 - 10) * (100vw - 320px) / (1440 - 320));
+  .instagram__description {
+    margin-top: 16px;
   }
 }
 </style>
---
+++ components/ui/Container.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="block-container">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+.block-container {
+  max-width: 1440px;
+  padding: 0px 60px;
+  margin: 0 auto;
+  width: 100%;
+  box-sizing: border-box;
+}
+@media screen and (max-width: 1280px) {
+  .block-container {
+    padding: 0px 50px;
+  }
+}
+@media screen and (max-width: 1024px) {
+  .block-container {
+    padding: 0px 40px;
+  }
+}
+@media screen and (max-width: 320px) {
+  .block-container {
+    padding: 0px 15px;
+  }
+}
+</style>
